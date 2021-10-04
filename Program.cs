330b14c0f2ecbe2e05673d99e75a8dfde013891d
chadashowalter <89471968+chadashowalter@users.noreply.github.com>
2021-10-03 21:48:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace recount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Dictionary<string, int> candidateNameCount = new Dictionary<string, int>();
+
+            int Winners = -1;
+            string WinningName = "";
+            int tieCount = 0;
+
+            string currentName = Console.ReadLine();
+
+            
+            while (currentName != "***")
+            {
+               
+                if (!candidateNameCount.ContainsKey(currentName))
+                {
+                    candidateNameCount.Add(currentName, 1);
+                }
+
+                else
+                {
+
+                    candidateNameCount[currentName] += 1;
+                }
+
+
+                
+                if (candidateNameCount[currentName] > Winners)
+                {
+                    
+                    Winners = candidateNameCount[currentName];
+                    WinningName = currentName;
+                    tieCount = 0;
+                }
+               
+                else if (candidateNameCount[currentName] == Winners)
+                {
+                 
+                    tieCount += 1;
+                }
+
+                currentName = Console.ReadLine();
+            }
+
+
+            if (tieCount > 0)
+            {
+                Console.WriteLine("Runoff!");
+            }
+            else
+            {
+                Console.WriteLine(WinningName);
+            }
+
+          
+
+        }
+    }
+}
